bcee2b62a689944813f90290ca9fd6b19e1e609a
Mohsin Ghani <Mohsinghani.777@gmail.com>
2018-04-08 19:54:39 +0500

    Create upload.php

---
+++ upload.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<!-Note: first create uploads folder in your root directory and if you change this file name eg: upload.php to index.php then you also need to change form action upload.php to index.php in line number 8-->
+<html>
+<head>
+  <title>Upload your files</title>
+</head>
+<body>
+  <form enctype="multipart/form-data" action="upload.php" method="POST">
+    <p>Upload your file</p>
+    <input type="file" name="uploaded_file"></input><br />
+    <input type="submit" value="Upload"></input>
+  </form>
+</body>
+</html>
+<?PHP
+  if(!empty($_FILES['uploaded_file']))
+  {
+    $path = "uploads/";
+    $path = $path . basename( $_FILES['uploaded_file']['name']);
+    echo $path;
+    if(move_uploaded_file($_FILES['uploaded_file']['tmp_name'], $path)) {
+      echo "The file ".  basename( $_FILES['uploaded_file']['name']). 
+      " has been uploaded";
+    } else{
+        echo "There was an error uploading the file, please try again!";
+    }
+  }
+?>
